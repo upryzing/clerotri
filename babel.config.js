07718027f3722216f93b2996401c8fce8a1d8e63
Rexogamer <beartechtalks@gmail.com>
2025-03-09 18:51:19 +0000

    chore: run prettier

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 const {mobilePlugins} = require('./config/babel-shared');
 
 module.exports = {
-  presets: [
-    [
-      '@rnx-kit/babel-preset-metro-react-native',
-    ],
-  ],
+  presets: [['@rnx-kit/babel-preset-metro-react-native']],
   env: {
     production: {
       plugins: [
