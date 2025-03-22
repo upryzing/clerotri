d14a6cbf8311e54429b770ac88d444d5064d3b33
Rexogamer <beartechtalks@gmail.com>
2025-03-22 00:45:43 +0000

    fix: correct style property

--- src/components/navigation/ChannelList.tsx
+++ src/components/navigation/ChannelList.tsx
@@ -330,7 +330,7 @@ const generateLocalStyles = (currentTheme: Theme, inset: number) => {
       backgroundColor: currentTheme.backgroundSecondary,
       flexGrow: 1000,
       flex: 1000,
-      ...(Platform.OS !== 'web' && {paddingTop: inset}),
+      ...(Platform.OS !== 'web' && {marginTop: inset}),
     },
   });
 };
