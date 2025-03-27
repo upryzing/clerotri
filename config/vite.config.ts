b4af525aa2b0920445070283feb2401d281a54e4
Rexogamer <beartechtalks@gmail.com>
2025-03-27 02:25:49 +0000

    chore: clean up unused dev deps

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -2,13 +2,12 @@ import {defineConfig} from 'vite';
 
 import react from '@vitejs/plugin-react';
 import reactNativeWeb from 'vite-plugin-react-native-web';
-import svgr from '@svgr/rollup';
 
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), reactNativeWeb(), svgr()],
+  plugins: [react(), reactNativeWeb()],
   resolve: {
     alias: {
       '@clerotri': path.resolve(__dirname, '../src'),
